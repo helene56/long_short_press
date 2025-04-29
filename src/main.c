35a84f4d7eb3c4d97c8d1acca9a9a738de6ca9d0
helene <helene.maja@hotmail.dk>
2025-04-29 23:49:04 +0200

    configure button as an input, and test button_pressed function

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <inttypes.h>
 
 /* 1000 msec = 1 sec */
-#define TIMER_MS   1000
+#define TIMER_MS   500
 
 
 // tasks
@@ -34,11 +35,11 @@ static const struct gpio_dt_spec LED2 = GPIO_DT_SPEC_GET(DT_NODELABEL(led1), gpi
 static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(DT_NODELABEL(button0), gpios);
 
 static struct gpio_callback button_cb_data;
-static uint64_t time_stamp;
 static uint64_t start_time;
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
+	// active low
 	bool pressed = gpio_pin_get_dt(&button);
 	if (pressed)
 	{
@@ -47,15 +48,23 @@ void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t
 	}
 	else
 	{
+		uint64_t held_time = k_uptime_get() - start_time;
 		// released! start figuring out which button to toggle
-		if (k_uptime_get() - start_time <= TIMER_MS)
+		if (held_time < TIMER_MS)
 		{
 			// turn led1 on/off
 			gpio_pin_toggle_dt(&LED1);
+			// gpio_pin_set(LED1.port, LED1.pin, 1);
+			printk("Toggle led1\n");
+			printk("Button was held for %" PRIu64 " ms\n", held_time);
+	
 		}
 		else
 		{
 			gpio_pin_toggle_dt(&LED2);
+			// gpio_pin_set(LED2.port, LED2.pin, 1);
+			printk("Toggle led2\n");
+			printk("Button was held for %" PRIu64 " ms\n", held_time);
 		}
 	}
 	
@@ -80,18 +89,23 @@ int main(void)
 		return -1;
 	}
 
-	ret = gpio_pin_configure_dt(&LED1, GPIO_OUTPUT_LOW);
+	ret = gpio_pin_configure_dt(&LED1, GPIO_OUTPUT_HIGH);
 	if (ret < 0) 
 	{
 		return -1;
 	}
 
-	ret = gpio_pin_configure_dt(&LED2, GPIO_OUTPUT_LOW);
+	ret = gpio_pin_configure_dt(&LED2, GPIO_OUTPUT_HIGH);
 	if (ret < 0) 
 	{
 		return -1;
 	}
 
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret < 0) 
+	{
+		return -1;
+	}
 	// configure button state
 	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_BOTH);
 	if (ret < 0) 
@@ -103,13 +117,7 @@ int main(void)
 	gpio_add_callback(button.port, &button_cb_data);
 
 	while (1) {
-		// ret = gpio_pin_toggle_dt(&led);
-		// if (ret < 0) {
-		// 	return 0;
-		// }
 
-		// led_state = !led_state;
-		// printf("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(TIMER_MS);
 	}
 	return 0;
