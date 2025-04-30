99ca952b00446d51c78d118213f7c036a498305e
helene <helene.maja@hotmail.dk>
2025-04-30 21:39:05 +0200

    tabs to spaces, remove comments, add comments

--- src/main.c
+++ src/main.c
@@ -39,86 +39,84 @@ static uint64_t start_time;
 
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
-	// active low
-	bool pressed = gpio_pin_get_dt(&button);
-	if (pressed)
-	{
-		// start measuring time
-		start_time = k_uptime_get();
-	}
-	else
-	{
-		uint64_t held_time = k_uptime_get() - start_time;
-		// released! start figuring out which button to toggle
-		if (held_time < TIMER_MS)
-		{
-			// turn led1 on/off
-			gpio_pin_toggle_dt(&LED1);
-			// gpio_pin_set(LED1.port, LED1.pin, 1);
-			printk("Toggle led1\n");
-			printk("Button was held for %" PRIu64 " ms\n", held_time);
-	
-		}
-		else
-		{
-			gpio_pin_toggle_dt(&LED2);
-			// gpio_pin_set(LED2.port, LED2.pin, 1);
-			printk("Toggle led2\n");
-			printk("Button was held for %" PRIu64 " ms\n", held_time);
-		}
-	}
-	
+
+    bool pressed = gpio_pin_get_dt(&button);
+    if (pressed)
+    {
+        // start measuring time
+        start_time = k_uptime_get();
+    }
+    else
+    {
+        uint64_t held_time = k_uptime_get() - start_time;
+        // released! start figuring out which button to toggle
+        if (held_time < TIMER_MS)
+        {
+            // turn led1 on/off
+            gpio_pin_toggle_dt(&LED1);
+            printk("Toggle led1\n");
+            printk("Button was held for %" PRIu64 " ms\n", held_time);
+    
+        }
+        else
+        {
+            gpio_pin_toggle_dt(&LED2);
+            printk("Toggle led2\n");
+            printk("Button was held for %" PRIu64 " ms\n", held_time);
+        }
+    }
+    
 }
 
 int main(void)
 {
-	int ret;
-	// check if gpios are ready
-	if (!gpio_is_ready_dt(&LED1)) 
-	{
-		return -1;
-	}
+    int ret;
+    // check if gpios are ready
+    if (!gpio_is_ready_dt(&LED1)) 
+    {
+        return -1;
+    }
 
-	if (!gpio_is_ready_dt(&button))
-	{
-		return -1;
-	}
+    if (!gpio_is_ready_dt(&button))
+    {
+        return -1;
+    }
 
-	if (!gpio_is_ready_dt(&LED2)) 
-	{
-		return -1;
-	}
+    if (!gpio_is_ready_dt(&LED2)) 
+    {
+        return -1;
+    }
+    // configure gpios
+    ret = gpio_pin_configure_dt(&LED1, GPIO_OUTPUT_HIGH);
+    if (ret < 0) 
+    {
+        return -1;
+    }
 
-	ret = gpio_pin_configure_dt(&LED1, GPIO_OUTPUT_HIGH);
-	if (ret < 0) 
-	{
-		return -1;
-	}
+    ret = gpio_pin_configure_dt(&LED2, GPIO_OUTPUT_HIGH);
+    if (ret < 0) 
+    {
+        return -1;
+    }
 
-	ret = gpio_pin_configure_dt(&LED2, GPIO_OUTPUT_HIGH);
-	if (ret < 0) 
-	{
-		return -1;
-	}
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (ret < 0) 
+    {
+        return -1;
+    }
+    // configure button state irs
+    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_BOTH);
+    if (ret < 0) 
+    {
+        return -1;
+    }
 
-	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-	if (ret < 0) 
-	{
-		return -1;
-	}
-	// configure button state
-	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_BOTH);
-	if (ret < 0) 
-	{
-		return -1;
-	}
+    gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin)); 	
+    gpio_add_callback(button.port, &button_cb_data);
 
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin)); 	
-	gpio_add_callback(button.port, &button_cb_data);
+    while (1) {
 
-	while (1) {
-
-		k_msleep(TIMER_MS);
-	}
-	return 0;
+        k_msleep(TIMER_MS);
+    }
+    return 0;
 }
